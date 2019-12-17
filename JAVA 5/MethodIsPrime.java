2b03ed5d995aa86754a25786b49ba4cfaf6388b9
SkoopatyWoop <57785553+SkoopatyWoop@users.noreply.github.com>
2019-12-17 18:27:02 -0500

    Add files via upload

---
+++ JAVA 5/MethodIsPrime.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class MethodIsPrime
+{
+   public static void main(String [] args)
+   {
+   Scanner obj1=new Scanner(System.in);
+   System.out.println("Enter the number for which you want to find number of factors");
+   int number= obj1.nextInt();
+   //int count=isPrime(number);
+   //System.out.println("The number of factors are" +count);
+   
+   // to find whether a number is prime or not
+   if (isPrime(number))
+   System.out.println("The given number is a prime number");
+   else
+   System.out.println("The given number is not a prime number");
+
+   }
+
+ //Returns how many factors the given number has.
+
+public static boolean isPrime(int number) 
+ {
+   int count = 0;
+   for (int i = 1; i <= number; i++) 
+   {
+      if (number % i == 0) 
+      {
+      count++;  // i is a factor of number
+      }
+    }
+    if (count==2)
+    return true;
+    else
+    return false;
+ }
+} 
+ 
