83e419a5a9b5338643572589f390645df7cff84c
lwubete3 <lwubete3@gatech.edu>
2022-12-03 16:38:08 -0500

    jquery for opacity on scroll

--- site/app.js
+++ site/app.js
@@ -1,6 +1,6 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
+        // console.log(entry)
         if (entry.isIntersecting) { //Intersecting the cleint's viewport
             entry.target.classList.add('show');
         } else {
@@ -11,3 +11,16 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el));
+
+$(window).scroll(function () {
+    var scrollTop = $(this).scrollTop();
+    $('.panel').css({
+        opacity: function () {
+            var elementHeight = $(this).height(),
+                opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
+
+            console.log("element height" + elementHeight);
+            return opacity;
+        }
+    });
+});
