df0fa3f16606b4b7e441a1908106a2b5551dab5f
lwubete3 <lwubete3@gatech.edu>
2022-12-03 17:16:20 -0500

    opacity + scale3d mock done

--- site/app.js
+++ site/app.js
@@ -19,8 +19,20 @@ $(window).scroll(function () {
             var elementHeight = $(this).height(),
                 opacity = ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2;
 
-            console.log("element height" + elementHeight);
+            // console.log("element height" + elementHeight);
+            // console.log("scroll top" + scrollTop);
+
             return opacity;
+        }
+    });
+
+    $('.panel').css({
+        transform: function () {
+            var elementHeight = $(this).height(),
+                scale = Math.min(1, ((1 - (elementHeight - scrollTop) / elementHeight) * 0.8) + 0.2);
+            scale *= 0.1;
+            console.log(scale);
+            return `scale3d(${0.9 + scale}, ${0.9 + scale}, ${1})`
         }
     });
 });
