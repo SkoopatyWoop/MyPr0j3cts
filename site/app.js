c342ba9d1db8eb96a188e40f137f19bdd3fc0926
lwubete3 <lwubete3@gatech.edu>
2022-12-11 21:41:43 -0500

    navbar fixing buggy disappearance when scrolling to top

--- site/app.js
+++ site/app.js
@@ -63,7 +63,7 @@ $(window).scroll(animateDivs);
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
+    if (prevScrollpos >= currentScrollPos || window.scrollY <= 50) {
         document.getElementsByClassName("navbar")[0].style.top = "0";
     } else {
         document.getElementsByClassName("navbar")[0].style.top = "-50px";
